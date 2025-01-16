0d4acea54b77bc99f9c7c205d3f2163431ab7e91
Alexandra Loveridge <alelov@noc.ac.uk>
2025-01-16 15:23:57 +0000

    adding debugging

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,6 +7,7 @@ import pytest
 from inflammation.models import daily_max
 from inflammation.models import daily_min
 from inflammation.models import daily_mean
+from inflammation.models import patient_normalise
 
 @pytest.mark.parametrize(
     "test, expected",
@@ -74,16 +75,30 @@ def test_daily_max():
 
     npt.assert_array_equal(daily_max(test_input), test_result)
 
-from inflammation.models import patient_normalise
 
 @pytest.mark.parametrize(
-    "test, expected",
+    "test, expected, expect_raises",
     [
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+        ...  # previous test cases here, with None for expect_raises, except for the next one - add ValueError
+        ...  # as an expected exception (since it has a negative input value)
+            (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+                [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+                None,
+        ),
     ])
-def test_patient_normalise(test, expected):
-    """Test normalisation works for arrays of one and positive integers.
-       Test with a relative and absolute tolerance of 0.01."""
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
 
-    result = patient_normalise(np.array([test]))
-    npt.assert_allclose(result, np.array([expected]), rtol=1e-2, atol=1e-2)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            result = patient_normalise(np.array(test))
+            npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
