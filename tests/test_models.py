9e7b6c53329f5d813b169d086f281a83e32edc8f
Alexandra Loveridge <alelov@noc.ac.uk>
2025-01-16 12:08:48 +0000

    add max and min parametrise tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,14 +2,36 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
-from inflammation.models import daily_mean
 from inflammation.models import daily_max
 from inflammation.models import daily_min
+from inflammation.models import daily_mean
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
+
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_max(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
-    
 
     test_input = np.array([[0, 0],
                            [0, 0],
